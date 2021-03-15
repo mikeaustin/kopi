a05b7f2dd434c041d0d5b46c097c7cc1344c4912
Mike Austin <mike_ekim@yahoo.com>
2021-03-15 13:47:20 -0700

    Add scope parameter to all visitors

--- bin/InterpreterVisitors.js
+++ bin/InterpreterVisitors.js
@@ -2,67 +2,63 @@
 // InterpreterVisitors.js
 //
 
-const visit = (visitors, node) => {
+const visit = (visitors, node, scope) => {
   if (visitors[node.constructor.name]) {
-    return visitors[node.constructor.name](node);
+    return visitors[node.constructor.name](node, scope);
   } else {
     throw new Error('No visitor for ' + node.constructor.name);
   }
 };
 
 class InterpreterVisitors {
-  constructor(scope) {
-    this.scope = scope;
-  }
-
-  ApplyExpression({ expr: { params, statements }, args }) {
-    const evaluatedArgs = visit(this, args);
-
-    this.scope = Object.create(this.scope, params.elements.reduce((scope, param, index) => ({
+  ApplyExpression({ expr: { params, statements }, args }, scope) {
+    const evaluatedArgs = visit(this, args, scope);
+    const functionScope = Object.create(scope, params.elements.reduce((scope, param, index) => ({
       ...scope,
       [param.name]: {
         value: evaluatedArgs[index]
       }
     }), {}));
 
-    return statements.reduce((_, statement) => visit(this, statement), undefined);
+    return statements.reduce((_, statement) => visit(this, statement, functionScope), undefined);
   }
 
-  OperatorExpression({ op, left, right }) {
+  OperatorExpression({ op, left, right }, scope) {
     switch (op) {
-      case '+': return visit(this, left) + visit(this, right);
-      case '-': return visit(this, left) - visit(this, right);
+      case '+': return visit(this, left, scope) + visit(this, right, scope);
+      case '-': return visit(this, left, scope) - visit(this, right, scope);
     }
   }
 
-  TupleExpression({ elements }) {
-    return elements.map(value => visit(this, value));
+  TupleExpression({ elements }, scope) {
+    return elements.map(value => visit(this, value, scope));
   }
 
-  FunctionExpression({ params, statements }) {
+  FunctionExpression({ params, statements }, scope) {
     return {
-      params: visit(this, params),
+      params: visit(this, params, scope),
       statements: statements
     };
   }
 
-  TuplePattern({ elements }) {
-    return elements.map(value => visit(this, value));
+  TuplePattern({ elements }, scope) {
+    return elements.map(value => visit(this, value, scope));
   }
 
   Literal({ value }) {
     return value;
   }
 
-  Identifier({ name }) {
-    if (!(name in this.scope)) {
+  Identifier({ name }, scope) {
+    if (!(name in scope)) {
       throw new Error(`Variable '${name}' is not defined`);
     }
 
-    return this.scope[name];
+    return scope[name];
   }
 }
 
 module.exports = {
-  default: InterpreterVisitors
+  default: InterpreterVisitors,
+  visit
 };
