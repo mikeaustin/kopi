4bbdb3e46b814e2b1470092284b1dc3f36a39d3b
Mike Austin <mike_ekim@yahoo.com>
2021-03-23 17:57:59 -0700

    Move classes to their own file; Add classes to globals

--- bin/InterpreterVisitors.js
+++ bin/InterpreterVisitors.js
@@ -2,74 +2,7 @@
 // InterpreterVisitors.js
 //
 
-class Tuple {
-  constructor(values) {
-    this.values = values;
-
-    values.forEach((value, index) => this[index] = value);
-  }
-
-  inspect() {
-    return `(${this.values.map(value => value.inspect()).join(', ')})`;
-  }
-
-  ['+'](that) {
-    return new Tuple(this.values.reduce((values, value, index) => (
-      values.push(typeof value === 'number'
-        ? value + that.values[index]
-        : value['+'](that.values[index])), values
-    ), []));
-  }
-
-  ['++'](that) {
-    return new Tuple(this.values.reduce((values, value, index) => (
-      values.push(value.concat(that.values[index])), values
-    ), []));
-  }
-}
-
-class Range {
-  constructor(from, to) {
-    this.from = from;
-    this.to = to;
-  }
-
-  *[Symbol.iterator]() {
-    for (let i = this.from; i < this.to; ++i) {
-      yield i;
-    }
-  }
-
-  inspect() {
-    return `${this.from.inspect()}..${this.to.inspect()}`;
-  }
-}
-
-class Function {
-  constructor(closure, params, statements) {
-    this.closure = closure;
-    this.params = params;
-    this.statements = statements;
-  }
-
-  kopiApply(evaluatedArgs, scope, visitors) {
-    // console.log('Function.kopiApply scope', scope);
-
-    const matches = this.params.match(evaluatedArgs);
-
-    const newScope = Object.setPrototypeOf(matches, this.closure);
-
-    return this.statements.reduce(({ value, scope }, statement) => {
-      const result = visitors.visit(statement, newScope);
-      console.trace('Function.koniApply scope', result.scope);
-
-      return {
-        value: result.value,
-        scope: { ...scope, ...result.scope }
-      };
-    }, { value: undefined, scope });
-  }
-}
+const { Tuple, Range, Function } = require('./classes');
 
 //
 
---
+++ bin/classes.js
@@ -0,0 +1,81 @@
+class Tuple {
+  constructor(values) {
+    this.values = values;
+
+    values.forEach((value, index) => this[index] = value);
+  }
+
+  inspect() {
+    return `(${this.values.map(value => value.inspect()).join(', ')})`;
+  }
+
+  ['+'](that) {
+    return new Tuple(this.values.reduce((values, value, index) => (
+      values.push(typeof value === 'number'
+        ? value + that.values[index]
+        : value['+'](that.values[index])), values
+    ), []));
+  }
+
+  ['++'](that) {
+    return new Tuple(this.values.reduce((values, value, index) => (
+      values.push(value.concat(that.values[index])), values
+    ), []));
+  }
+}
+
+class Range {
+  constructor(from, to) {
+    this.from = from;
+    this.to = to;
+  }
+
+  *[Symbol.iterator]() {
+    for (let i = this.from; i < this.to; ++i) {
+      yield i;
+    }
+  }
+
+  inspect() {
+    return `${this.from.inspect()}..${this.to.inspect()}`;
+  }
+}
+
+Range.kopiApply = (args, scope) => {
+  return {
+    value: args,
+    scope
+  };
+};
+
+class Function {
+  constructor(closure, params, statements) {
+    this.closure = closure;
+    this.params = params;
+    this.statements = statements;
+  }
+
+  kopiApply(evaluatedArgs, scope, visitors) {
+    // console.log('Function.kopiApply scope', scope);
+
+    const matches = this.params.match(evaluatedArgs);
+
+    const newScope = Object.setPrototypeOf(matches, this.closure);
+
+    return this.statements.reduce(({ value, scope }, statement) => {
+      const result = visitors.visit(statement, newScope);
+      console.trace('Function.koniApply scope', result.scope);
+
+      return {
+        value: result.value,
+        scope: { ...scope, ...result.scope }
+      };
+    }, { value: undefined, scope });
+  }
+}
+
+module.exports = {
+  Tuple,
+  Range,
+  Function
+};
