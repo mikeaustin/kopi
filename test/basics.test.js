95f13bef4d384ad7466ad97a773f511740b1a15b
Mike Austin <mike_ekim@yahoo.com>
2021-03-31 19:48:15 -0700

    Add more type checking

--- test/basics.test.js
+++ test/basics.test.js
@@ -10,13 +10,13 @@ let visitors = new InterpreterVisitors();
 let printASTVisitors = new PrintASTVisitors();
 let scope = functions;
 
-const ApplyNonFunction =
+// const ApplyNonFunction =
 
-  test('cps', () => {
-    let ast = parser.parse('(x, k => (x, k => k x + 1) x + x, k) 5, (x => x)');
+test('cps', () => {
+  let ast = parser.parse('(x, k => (x, k => k x + 1) x + x, k) 5, (x => x)');
 
-    expect(visitors.visit(ast.statements[0], scope).value).toEqual(11);
-  });
+  expect(visitors.visit(ast.statements[0], scope).value).toEqual(11);
+});
 
 test('factorial', () => {
   var ast = parser.parse(`
