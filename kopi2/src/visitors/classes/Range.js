287c7afd199d82ae50023558049124990a87b210
Mike Austin <mike_ekim@yahoo.com>
2021-04-18 17:39:16 -0700

    Add valueForField(); Add math ops; Add Range map

--- kopi2/src/visitors/classes/Range.js
+++ kopi2/src/visitors/classes/Range.js
@@ -4,10 +4,6 @@ class Range {
   constructor(from, to) {
     this.from = from;
     this.to = to;
-
-    for (let index = 0; index < this.to - this.from + 1; ++index) {
-      this[index] = index + this.from;
-    };
   }
 
   escape() {
