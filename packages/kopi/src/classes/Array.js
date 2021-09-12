79864aebccfb4a711b6e2ca68365fe44e6ff167f
Mike Austin <mike_ekim@yahoo.com>
2021-09-11 18:59:21 -0700

    Add KopiArray

--- packages/kopi/src/classes/Array.js
+++ packages/kopi/src/classes/Array.js
@@ -1,8 +1,29 @@
 const util = require("util");
 
-class Array {
+class KopiArray {
+  constructor(elements = []) {
+    this.elements = elements;
+  }
+
+  toString = function () {
+    return `[${this.elements.map(element => element.toString()).join(', ')}]`;
+  };
+
+  [util.inspect.custom]() {
+    return `[${this.elements.map(element => inspect(element)).join(', ')}]`;
+  }
+
+  *[Symbol.iterator]() {
+    return this.elements[Symbol.iterator]();
+  }
+
+  map(args, scope, visitors) {
+    return Promise.all(this.elements.map((element) => (
+      args.apply(undefined, [element, scope, visitors])
+    )));
+  }
 }
 
 module.exports = {
-  default: Array,
+  default: KopiArray,
 };
