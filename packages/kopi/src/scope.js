4b1136586539cae130993ef6ad8e7225eee01c73
Mike Austin <mike_ekim@yahoo.com>
2021-09-13 17:34:53 -0700

    Rename built-in function arguments

--- packages/kopi/src/scope.js
+++ packages/kopi/src/scope.js
@@ -7,18 +7,19 @@ const { KopiTuple, KopiVector } = require('./classes');
 const target = new EventEmitter();
 
 let getScope = (input) => ({
+  id: (x) => x,
   at: (index) => async array => await array[index],
   import: (args) => 0,
-  number: (args) => Number(args.value),
-  char: (args) => String.fromCodePoint(args),
-  string: (args) => String(args),
-  print: (args) => console.log(args.toString()),
-  write: (args) => new Promise(resolve => process.stdout.write(args.toString(), () => resolve())),
-  sleep: (args) => new Promise(resolve => setTimeout(() => resolve(args), args * 1000)),
-  fetch: (args) => fetch(args).then(data => data.headers.get('content-type')),
-  loop: async (args, scope, visitors) => {
+  number: ({ value }) => Number(value),
+  char: (num) => String.fromCodePoint(num),
+  string: (num) => String(num),
+  print: (val) => console.log(val.toString()),
+  write: (val) => new Promise(resolve => process.stdout.write(val.toString(), () => resolve())),
+  sleep: (secs) => new Promise(resolve => setTimeout(() => resolve(secs), secs * 1000)),
+  fetch: (url) => fetch(url).then(data => data.headers.get('content-type')),
+  loop: async (fn, scope, visitors) => {
     const exit = () => { throw -1; };
-    const func = await args.apply(undefined, [exit, scope, visitors]);
+    const func = await fn.apply(undefined, [exit, scope, visitors]);
 
     let value = KopiTuple.empty;
 
@@ -31,19 +32,19 @@ let getScope = (input) => ({
     }
     loop(value);
   },
-  spawn: (args, scope, visitors) => {
-    args.apply(undefined, [KopiTuple.empty, scope, visitors]);
+  spawn: (fn, scope, visitors) => {
+    fn.apply(undefined, [KopiTuple.empty, scope, visitors]);
   },
-  yield: (args, scope, visitors) => {
+  yield: (fn, scope, visitors) => {
     return new Promise(resolve => {
       target.once('message', (event) => {
-        event.value = args.apply(undefined, [event.data, scope, visitors]);
+        event.value = fn.apply(undefined, [event.data, scope, visitors]);
 
         resolve(event.value);
       });
     });
   },
-  send: (args) => (data) => {
+  send: (cid) => (data) => {
     return new Promise(resolve => setImmediate(() => {
       const event = { data };
       target.emit('message', event);
@@ -51,26 +52,26 @@ let getScope = (input) => ({
       resolve(event.value);
     }));
   },
-  random: (argss) => Math.random(),
+  random: () => Math.random(),
   time: () => new Date().toLocaleTimeString(),
-  repeat: (args, scope, visitors) => (
+  repeat: (fn, scope, visitors) => (
     function next(value) {
       if (value?.elements?.length === 0) {
         value = 1;
       }
-      const nextValue = args.apply(undefined, [value, scope, visitors]);
+      const nextValue = fn.apply(undefined, [value, scope, visitors]);
 
       return new KopiTuple([nextValue, () => next(nextValue)]);
     }
   ),
-  input: (args) => {
+  input: (str) => {
     const rl = input ?? readline.createInterface({
       input: process.stdin,
       output: process.stdout
     });
 
     return new Promise(resolve => {
-      rl.question(`${args} `, data => {
+      rl.question(`${str} `, data => {
         if (rl !== input) {
           rl.close();
         }
@@ -79,13 +80,13 @@ let getScope = (input) => ({
       });
     });
   },
-  Vector: (args) => new KopiVector(args.elements[0], args.elements[1]),
-  even: (args) => args % 2 === 0,
-  max: (args) => Math.max(args.elements[0], args.elements[1]),
-  let: (args, scope, visitors) => {
-    return args.apply(undefined, [KopiTuple.empty, scope, visitors]);
+  Vector: (tuple) => new KopiVector(tuple.elements[0], tuple.elements[1]),
+  even: (num) => num % 2 === 0,
+  max: (tuple) => Math.max(tuple.elements[0], tuple.elements[1]),
+  let: (fn, scope, visitors) => {
+    return fn.apply(undefined, [KopiTuple.empty, scope, visitors]);
   },
-  do: (args, scope) => interpreter.visitNode(args, scope),
+  // do: (args, scope) => interpreter.visitNode(args, scope),
   match: (value, scope, visitors) => (funcs) => {
     for (func of funcs.elements) {
       if (func.params.getMatches(value)) {
