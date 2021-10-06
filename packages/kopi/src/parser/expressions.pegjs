e203c6fd9a02361caa312ab4b93f892400fd7201
Mike Austin <mike_ekim@yahoo.com>
2021-10-06 14:40:01 -0700

    Add statements.pegjs; Clean up code

--- packages/kopi/src/parser/expressions.pegjs
+++ packages/kopi/src/parser/expressions.pegjs
@@ -1,21 +1,3 @@
-Block
-  = Newline* head:Statement? tail:(Newline+ Statement)* Newline* {
-      return new Block({
-        statements: tail.reduce((block, [, statement]) => (
-          statement ? [...block, statement] : block
-        ), [head])
-      });
-    }
-
-Statement
-  = Assignment
-  / Expression
-
-Assignment
-  = pattern:Pattern _ "=" !">" _ expr:Expression {
-      return new Assignment({ pattern, expr })
-    }
-
 Expression
   = NextRule
 
--- packages/kopi/src/parser/rules2.js
+++ packages/kopi/src/parser/rules2.js
@@ -1,14 +1,21 @@
 const util = require('util');
 const fs = require('fs');
 
+async function readFiles(...files) {
+  return Promise.all(files.map(file => util.promisify(fs.readFile)(file, 'utf-8')));
+}
+
 async function main() {
   const functions = await util.promisify(fs.readFile)('./src/parser/functions.js', 'utf-8');
-  const expressions = await util.promisify(fs.readFile)('./src/parser/expressions.pegjs', 'utf-8');
-  const operators = await util.promisify(fs.readFile)('./src/parser/operators.pegjs', 'utf-8');
-  const patterns = await util.promisify(fs.readFile)('./src/parser/patterns.pegjs', 'utf-8');
-  const terminals = await util.promisify(fs.readFile)('./src/parser/terminals.pegjs', 'utf-8');
+  const [statements, expressions, operators, patterns, terminals] = await readFiles(
+    './src/parser/statements.pegjs',
+    './src/parser/expressions.pegjs',
+    './src/parser/operators.pegjs',
+    './src/parser/patterns.pegjs',
+    './src/parser/terminals.pegjs',
+  );
 
-  const rulesString = [expressions, operators, patterns, terminals].join('\n');
+  const rulesString = [statements, expressions, operators, patterns, terminals].join('\n');
 
   const rules = rulesString.split(/\r?\n\r?\n/).reduce((rulesString, rule) => {
     const index = rule.search(/\r?\n/);
@@ -17,7 +24,6 @@ async function main() {
       ...rulesString,
       [rule.slice(0, index)]: rule.slice(index + 1)
     };
-
   }, {});
 
   const orderedOperatorRules = [
