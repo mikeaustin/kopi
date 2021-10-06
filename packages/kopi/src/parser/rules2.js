a6dc74459873f3274d256f2546f00bfcf54b7935
Mike Austin <mike_ekim@yahoo.com>
2021-10-06 12:35:39 -0700

    Rename variables

--- packages/kopi/src/parser/rules2.js
+++ packages/kopi/src/parser/rules2.js
@@ -8,52 +8,58 @@ async function main() {
   const patterns = await util.promisify(fs.readFile)('./src/parser/patterns.pegjs', 'utf-8');
   const terminals = await util.promisify(fs.readFile)('./src/parser/terminals.pegjs', 'utf-8');
 
-  const rules = [expressions, operators, patterns, terminals].join('\n');
+  const rulesString = [expressions, operators, patterns, terminals].join('\n');
 
-  const nonTerminals = rules.split(/\r?\n\r?\n/).reduce((rules, rule) => {
+  const rules = rulesString.split(/\r?\n\r?\n/).reduce((rulesString, rule) => {
     const index = rule.search(/\r?\n/);
 
     return {
-      ...rules,
+      ...rulesString,
       [rule.slice(0, index)]: rule.slice(index + 1)
     };
 
   }, {});
 
-  console.error(nonTerminals);
-
-  const orderedRules = [
+  const orderedExpressionRules = [
     'Expression',
     'TupleExpression',
     'AddExpression',
     'MultiplyExpression',
     'ApplyExpression',
     'PrimaryExpression',
+  ];
 
+  const orderedPatternRules = [
     "Pattern",
     "TuplePattern",
     "PrimaryPattern",
     "IdentifierPattern",
+  ];
 
+  const orderedTerminalRules = [
     'NumericLiteral',
     'Identifier',
-
     '_',
     'Whitespace',
     'Newline',
   ];
 
-  const orderedNonTerminals = orderedRules.reverse().reduce(([orderedNonTerminals, nextRule], rule) => (
-    [[...orderedNonTerminals, rule + '\n' + nonTerminals[rule].replace(/NextRule/g, nextRule)], rule]
+  const orderedRules = [
+    ...orderedExpressionRules,
+    ...orderedPatternRules,
+    ...orderedTerminalRules,
+  ];
+
+  const orderedRulesString = orderedRules.reverse().reduce(([orderedRulesString, nextRule], rule) => (
+    [[...orderedRulesString, rule + '\n' + rules[rule].replace(/NextRule/g, nextRule)], rule]
   ), [[], null])[0].reverse();
 
   console.log(
     '{\n' +
     functions +
     '}\n\n' +
-    orderedNonTerminals.join('\n\n')
+    orderedRulesString.join('\n\n')
   );
 }
-
 
 main();
