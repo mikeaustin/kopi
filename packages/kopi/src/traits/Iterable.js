a8202cd67b8a74a27b44316e6c93ea2b0213897a
Mike Austin <mike_ekim@yahoo.com>
2021-11-07 08:20:55 -0800

    Update Iterable.js

--- packages/kopi/src/traits/Iterable.js
+++ packages/kopi/src/traits/Iterable.js
@@ -5,8 +5,8 @@ class Iterable {
   async map(func, scope, visitors) {
     let accum = this.emptyValue();
 
-    for (const element of this) {
-      accum = accum.concat(await func.apply(undefined, [await element, scope, visitors]));
+    for await (const element of this) {
+      accum = accum.concat(await func.apply(undefined, [element, scope, visitors]));
     }
 
     return accum;
@@ -21,7 +21,7 @@ class Iterable {
   async flatMap(func, scope, visitors) {
     let accum = this.emptyValue();
 
-    for (const element of this) {
+    for await (const element of this) {
       const appliedElement = await func.apply(undefined, [element, scope, visitors]);
 
       if (appliedElement[Symbol.iterator]) {
@@ -39,8 +39,8 @@ class Iterable {
       let accum = init;
       let index = 0;
 
-      for (const element of this) {
-        accum = await func.apply(undefined, [new KopiTuple([accum, await element, index++]), scope, visitors]);
+      for await (const element of this) {
+        accum = await func.apply(undefined, [new KopiTuple([accum, element, index++]), scope, visitors]);
       }
 
       return accum;
@@ -52,7 +52,7 @@ class Iterable {
     const accum = [];
     let values = [];
 
-    for (const element of this) {
+    for await (const element of this) {
       if (delimiterRexExp.test(element.valueOf())) {
         if (values.length > 0) {
           accum.push(values);
@@ -70,6 +70,24 @@ class Iterable {
 
     return accum;
   }
+  
+  splitEvery(count) {
+    const accum = [];
+    let values = []
+    
+    for await (const element of this) {
+      if (index % count === 0) {
+        accum.push(values);
+      } else {
+        values.push(element);
+      }
+    }
+    
+    if (valies.length !== 0) {
+      accum.push(values);
+    }
+    
+    return accum;
 }
 
 module.exports = {
