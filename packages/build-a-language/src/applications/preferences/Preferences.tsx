084b05169a4c79cef9e26bef5404f67e9ab52c65
Mike Austin <mike_ekim@yahoo.com>
2022-02-01 12:42:27 -0800

    Delete more images; Save background to localStorage

--- packages/build-a-language/src/applications/preferences/Preferences.tsx
+++ packages/build-a-language/src/applications/preferences/Preferences.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 
-import { View, Button, Spacer, List, Clickable } from '../../components';
+import { View, Text, Button, Spacer, Divider, List, Clickable } from '../../components';
 
 import Image, { type ImageProps } from '../../components/image';
 
@@ -33,25 +33,45 @@ type PreferencesProps = {
 const Preferences = ({
   onSetBackground
 }: PreferencesProps) => {
+  const backgroundRef = useRef<string>();
+
   const handleImageClick = (event: any, src: string) => {
+    backgroundRef.current = src;
+
     onSetBackground(src);
   };
 
+  const handleSaveClick = () => {
+    localStorage.setItem('background', JSON.stringify(backgroundRef.current));
+  };
+
   useEffect(() => {
-    onSetBackground('./images/triangles-colorful-green-colors-low-poly-abstract-4748.png');
-  }, []);
+    const background = localStorage.getItem('background');
+
+    if (background) {
+      backgroundRef.current = JSON.parse(background);
+
+      onSetBackground(JSON.parse(background));
+    } else {
+      onSetBackground('./images/triangles-colorful-green-colors-low-poly-abstract-4748.png');
+    }
+  }, [onSetBackground]);
 
   return (
     <View padding="medium">
-      <View style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gap: '10px 10px', maxHeight: 300, overflowY: 'auto' }}>
+      <Text fontWeight="bold">Background</Text>
+      <Spacer size="medium" />
+      <Divider />
+      <View style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gap: '10px 10px', maxHeight: 300, overflowY: 'auto', paddingTop: 10, paddingBottom: 10 }}>
         {backgroundUrls.map((url, index) => (
           <ClickableImage key={index} src={`./images/${url}`} width="100%" height="auto" onImageClick={handleImageClick} />
         ))}
       </View>
+      <Divider />
       <Spacer size="medium" />
       <List horizontal spacerSize="small" justifyContent="center">
         <Button primary title="Cancel" />
-        <Button primary solid title="Save" />
+        <Button primary solid title="Save" onClick={handleSaveClick} />
       </List>
     </View>
   );
--- packages/build-a-language/src/applications/preferences/data.js
+++ packages/build-a-language/src/applications/preferences/data.js
@@ -1,21 +1,13 @@
 const data = [
   '1638098.jpg',
-  '1638137.png',
-  '1638435.jpg',
   '16930.jpg',
   '16933.jpg',
-  '16935.jpg',
-  '16989.jpg',
   '2909224.jpg',
   '2909247.jpg',
   '306406.jpg',
   '306409.jpg',
-  '306428.jpg',
-  '306440.png',
   '306458.png',
-  '306619.jpg',
   '3401069.jpg',
-  '346752.png',
   '6414167.jpg',
   '6414830.png',
   '653899.jpg',
@@ -24,7 +16,6 @@ const data = [
   '653936.jpg',
   '781767.jpg',
   '961115.jpg',
-  'purple-skies-3440x1440.jpg',
   'triangles-colorful-green-colors-low-poly-abstract-4748.png'
 ];
 
