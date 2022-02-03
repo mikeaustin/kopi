e838c851f7465dc99b2959943f266a02e623622a
Mike Austin <mike_ekim@yahoo.com>
2022-02-03 10:03:55 -0800

    Change Preferences style

--- packages/build-a-language/src/applications/preferences/Preferences.tsx
+++ packages/build-a-language/src/applications/preferences/Preferences.tsx
@@ -72,20 +72,20 @@ const Preferences = ({
 
   return (
     <View>
-      <View padding="medium" bottomPadding="none">
+      <View padding="medium" bottomPadding="none" background="gray-1">
         <Text fontWeight="bold">Background</Text>
         <Spacer size="medium" />
-        <Divider />
       </View>
+      <Divider style={{ marginBottom: -1, position: 'relative', zIndex: 1, background: 'hsla(0, 0%, 0%, 0.15)' }} />
       <View ref={scrollerRef} className={styles.outerScroll}>
-        <View padding="small" horizontalPadding="medium" className={styles.scroll} style={{ height: 305 }} onScroll={handleScroll}>
+        <View padding="medium" horizontalPadding="medium" className={styles.scroll} style={{ height: 305 }} onScroll={handleScroll}>
           {backgroundUrls.map((url, index) => (
             <ClickableImage key={index} src={`./images/${url}`} width="100%" height="auto" onImageClick={handleImageClick} />
           ))}
         </View>
       </View>
-      <View padding="medium" topPadding="none">
-        <Divider />
+      <Divider style={{ marginTop: -1, position: 'relative', zIndex: 1, background: 'hsla(0, 0%, 0%, 0.15)' }} />
+      <View padding="medium" topPadding="none" background="gray-1">
         <Spacer size="medium" />
         <List horizontal spacerSize="small" justifyContent="center">
           <Button primary title="Cancel" />
