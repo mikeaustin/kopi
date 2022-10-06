87c0d89c120041f77cc05c71887cf878045024dd
Mike Austin <mike_ekim@yahoo.com>
2022-10-06 07:55:02 -0700

    Add S3 Explorer client

--- clients/explorer/src/App.tsx
+++ clients/explorer/src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import AWS from 'aws-sdk';
+import { PromiseResult } from 'aws-sdk/lib/request';
 
 import { View, Text, Stack, Spacer, Divider, Icon } from 'core';
 
@@ -11,74 +12,83 @@ AWS.config.credentials = new AWS.Credentials({
   secretAccessKey: 'CbeKGABrL06QaYihVmpXuIExuzfGVn+pbWgBV18r',
 });
 
-interface Folder {
-  folders?: AWS.S3.CommonPrefixList,
-  files?: AWS.S3.ObjectList,
-  expanded: boolean,
-  chidren?: Folder,
-}
+const s3 = new AWS.S3({
+  params: {
+    Bucket: 'https://mike-austin.s3.amazonaws.com/photos'
+  }
+});
 
-const Entry = ({
-  path
-}: {
-  path: string,
-}) => {
-  const [objects, setObjects] = useState<Folder | null>(null);
-
-  useEffect(() => {
-    (async () => {
-      const s3 = new AWS.S3({
-        params: {
-          // Bucket: 'https://mike-austin.s3.amazonaws.com/photos'
-        }
-      });
-
-      const objects = await s3.listObjectsV2({
-        Bucket: 'mike-austin',
-        Prefix: 'photos/',
-        StartAfter: 'photos/',
-        Delimiter: '/',
-      }).promise();
-
-      console.log(objects);
-
-      setObjects({
-        files: objects.Contents?.map(file => ({
-          ...file,
-          Key: file.Key?.split('/')[1]
-        })),
-        folders: objects.CommonPrefixes?.map(file => ({
-          ...file,
-          Prefix: file.Prefix?.split('/')[1]
-        })),
-        expanded: false,
-      });
-    })();
-  }, []);
+const Folder = ({ Prefix, level }: AWS.S3.CommonPrefix & { level: number; }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
 
   const handleFolderExpandClick = () => {
-
+    setIsExpanded(isExpanded => !isExpanded);
   };
 
   return (
-    <Stack flex padding="small medium" spacing="small" fillColor="white">
-      {objects?.folders?.map((item) => (
-        <Stack key={item.Prefix} horizontal>
-          <Icon icon="chevron-right" style={{ cursor: 'pointer' }} onClick={handleFolderExpandClick} />
-          <Icon icon="folder" />
-          <Spacer size="xsmall" />
-          <Text>{item.Prefix}</Text>
-        </Stack>
+    <View >
+      <Stack key={Prefix} horizontal align="left">
+        <Icon icon="chevron-right" size="xs" style={{ cursor: 'pointer', marginLeft: level * 20 }} rotation={isExpanded ? 90 : undefined} onClick={handleFolderExpandClick} />
+        <Icon icon="folder" />
+        <Spacer size="xsmall" />
+        <Text>{Prefix?.split('/').at(-2)}</Text>
+      </Stack>
+      {isExpanded && (
+        <>
+          <Spacer size="small" />
+          <Entry path={Prefix ?? ''} level={level + 1} />
+        </>
+      )}
+    </View>
+  );
+};
+
+const File = ({ Key, Size, LastModified, level }: AWS.S3.Object & { level: number; }) => {
+  return (
+    <Stack horizontal align="left">
+      <Icon icon="file" style={{ marginLeft: level * 20 + 20 }} />
+      <Spacer size="xsmall" />
+      <Text style={{ width: 300 - (level * 20) }}>{Key?.split('/').at(-1)}</Text>
+      <Text style={{ width: 150 }}>{Size}</Text>
+      <Text>{LastModified?.toLocaleDateString()}</Text>
+    </Stack>
+  );
+};
+
+interface EntryProps extends React.ComponentProps<typeof View> {
+  path: string,
+  level?: number,
+}
+
+const Entry = ({
+  path,
+  level = 0,
+  ...props
+}: EntryProps) => {
+  const [objects, setObjects] = useState<PromiseResult<AWS.S3.ListObjectsV2Output, AWS.AWSError> | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      const objects = await s3.listObjectsV2({
+        Bucket: 'mike-austin',
+        Prefix: path,
+        StartAfter: path,
+        Delimiter: '/',
+      }).promise();
+
+      // console.log(objects);
+
+      setObjects(objects);
+    })();
+  }, [path]);
+
+  return (
+    <Stack flex spacing="small" {...props}>
+      {objects?.CommonPrefixes?.map((item) => (
+        <Folder level={level} key={item.Prefix} {...item} />
       ))}
-      {objects?.files?.map((item) => (
-        <Stack key={item.Key} horizontal align="left">
-          <View style={{ width: 20 }} />
-          <Icon icon="file" />
-          <Spacer size="xsmall" />
-          <Text style={{ width: 300 }}>{item.Key}</Text>
-          <Text style={{ width: 150 }}>{item.Size}</Text>
-          <Text>{item.LastModified?.toLocaleDateString()}</Text>
-        </Stack>
+      {objects?.Contents?.map((item) => (
+        <File level={level} key={item.Key} {...item} />
       ))}
     </Stack>
   );
@@ -94,7 +104,7 @@ function App() {
         <Text light caps fontSize="xsmall" fontWeight="bold">Last Modified</Text>
       </Stack>
       <Divider />
-      <Entry path={'photos/'} />
+      <Entry path={'photos/'} padding="small medium" fillColor="white" />
     </View>
   );
 }
