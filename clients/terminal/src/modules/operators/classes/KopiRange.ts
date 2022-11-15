ea0dc3bffd4bbdab9c03a881f147c7b876888095
Mike Austin <mike_ekim@yahoo.com>
2022-11-14 16:45:18 -0800

    Change Range by to stride

--- clients/terminal/src/modules/operators/classes/KopiRange.ts
+++ clients/terminal/src/modules/operators/classes/KopiRange.ts
@@ -5,23 +5,25 @@ import { KopiNumber } from '../../terminals/classes';
 
 import KopiIterable from '../traits/KopiIterable';
 
-const assertTrait = async (value: KopiValue, variableName: string, trait: Function, errors: string[]) => {
-  const traits = (value.constructor as typeof KopiValue).traits;
+const assertTrait = async (value: KopiValue, variableName: string, traits: Function[], errors: string[]) => {
+  for (const trait of traits) {
+    const constructorTraits = (value.constructor as typeof KopiValue).traits;
 
-  if (!traits.includes(trait)) {
-    errors.push(`'${variableName}' value '${await value.inspect()}' is missing trait '${trait.constructor.name}'`);
+    if (!constructorTraits.includes(trait)) {
+      errors.push(`'${variableName}' value '${await value.inspect()}' is missing trait '${trait.constructor.name}'`);
+    }
   }
 };
 
 class KopiRange extends KopiValue {
   static override traits = [Applicative];
 
-  constructor(from: Promise<KopiValue>, to: Promise<KopiValue>, by?: Promise<KopiNumber>) {
+  constructor(from: Promise<KopiValue>, to: Promise<KopiValue>, stride?: Promise<KopiNumber>) {
     super();
 
     this.from = from;
     this.to = to;
-    this.by = by ?? Promise.resolve(new KopiNumber(1));
+    this.stride = stride ?? Promise.resolve(new KopiNumber(1));
   }
 
   override async inspect() {
@@ -38,22 +40,20 @@ class KopiRange extends KopiValue {
 
     let errors: string[] = [];
 
-    assertTrait(from, 'from', Enumerable, errors);
-    assertTrait(from, 'from', Comparable, errors);
-    assertTrait(_to, 'to', Enumerable, errors);
-    assertTrait(_to, 'to', Comparable, errors);
+    assertTrait(from, 'from', [Enumerable, Comparable], errors);
+    assertTrait(_to, 'to', [Enumerable, Comparable], errors);
 
     if (errors.length > 0) {
       throw new Error(`Range.iterator(): 'from' or 'to' values are missing traits:\n${errors.join('\n')}`);
     }
 
-    const to = (_to as unknown as Enumerable).succ(await this.by);
+    const to = (_to as unknown as Enumerable).succ(await this.stride);
 
     for (
       let current = from;
       ((current as unknown as Comparable).compare.apply(current, [to]) as KopiNumber).value < 0;
       // ((current as unknown as Comparable)['<'].apply(new KopiTuple([]), [to]) as KopiBoolean).value;
-      current = (current as unknown as Enumerable).succ(await this.by)
+      current = (current as unknown as Enumerable).succ(await this.stride)
     ) {
       yield current;
     }
@@ -61,7 +61,7 @@ class KopiRange extends KopiValue {
 
   from: Promise<KopiValue>;
   to: Promise<KopiValue>;
-  by: Promise<KopiNumber>;
+  stride: Promise<KopiNumber>;
 }
 
 for (const name of Object.getOwnPropertyNames(KopiIterable.prototype)) {
