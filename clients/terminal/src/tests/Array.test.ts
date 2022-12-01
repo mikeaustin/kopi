c7aeaecd7038126ddebccf0c9a463df0adc16081
Mike Austin <mike_ekim@yahoo.com>
2022-12-01 09:38:06 -0800

    Fix test indentation

--- clients/terminal/src/tests/Array.test.ts
+++ clients/terminal/src/tests/Array.test.ts
@@ -13,8 +13,8 @@ describe('Array', () => {
 
   test('Basics', async () => {
     var array = await interpret(`
-    [1, 2, 3]
-  `) as KopiArray;
+      [1, 2, 3]
+    `) as KopiArray;
 
     expect(array).toEqual(new KopiArray([
       new KopiNumber(1),
@@ -23,12 +23,12 @@ describe('Array', () => {
     ]));
 
     var array = await interpret(`
-    [
-      1
-      2
-      3
-    ]
-  `) as KopiArray;
+      [
+        1
+        2
+        3
+      ]
+    `) as KopiArray;
 
     expect(array).toEqual(new KopiArray([
       new KopiNumber(1),
@@ -37,41 +37,40 @@ describe('Array', () => {
     ]));
 
     var number = await interpret(`
-    'size [1, 2, 3]
-  `) as KopiNumber;
+      'size [1, 2, 3]
+    `) as KopiNumber;
 
     expect(number.value).toEqual(3);
 
     var boolean = await interpret(`
-    [1, 2, 3] | has 1
-  `) as KopiBoolean;
+      [1, 2, 3] | has 1
+    `) as KopiBoolean;
 
     expect(boolean).toEqual(new KopiBoolean(true));
 
     var boolean = await interpret(`
-    [1, 2, 3] | has 3
-  `) as KopiBoolean;
+      [1, 2, 3] | has 3
+    `) as KopiBoolean;
 
     expect(boolean).toEqual(new KopiBoolean(false));
 
-
     var boolean = await interpret(`
-    [1, 2, 3] | includes 2
-  `) as KopiBoolean;
+      [1, 2, 3] | includes 2
+    `) as KopiBoolean;
 
     expect(boolean).toEqual(new KopiBoolean(true));
   });
 
   test('Get', async () => {
     var number = await interpret(`
-    [1, 2, 3] 1
-  `) as KopiNumber;
+      [1, 2, 3] 1
+    `) as KopiNumber;
 
     expect(number).toEqual(new KopiNumber(2));
 
     var array = await interpret(`
-    [1, 2, 3] [1, 2]
-  `) as KopiArray;
+      [1, 2, 3] [1, 2]
+    `) as KopiArray;
 
     expect(array).toEqual(new KopiArray([
       new KopiNumber(2),
@@ -79,32 +78,31 @@ describe('Array', () => {
     ]));
 
     var array = await interpret(`
-    [1, 2, 3] 1..3
-  `) as KopiArray;
+      [1, 2, 3] 1..3
+    `) as KopiArray;
 
     expect(array).toEqual(new KopiArray([
       new KopiNumber(2),
       new KopiNumber(3),
     ]));
 
-
     var number = await interpret(`
-    [1, 2, 3] | get 1
-  `) as KopiNumber;
+      [1, 2, 3] | get 1
+    `) as KopiNumber;
 
     expect(number).toEqual(new KopiNumber(2));
 
     var number = await interpret(`
-    [1, 2, 3] | get 3
-  `) as KopiNumber;
+      [1, 2, 3] | get 3
+    `) as KopiNumber;
 
     expect(number).toEqual(KopiTuple.empty);
   });
 
   test('Set', async () => {
     var array = await interpret(`
-    [1, 2, 3] | set 1 5
-  `) as KopiArray;
+      [1, 2, 3] | set 1 5
+    `) as KopiArray;
 
     expect(array).toEqual(new KopiArray([
       new KopiNumber(1),
@@ -113,8 +111,8 @@ describe('Array', () => {
     ]));
 
     var array = await interpret(`
-    [1, 2, 3] | set 4 5
-  `) as KopiArray;
+      [1, 2, 3] | set 4 5
+    `) as KopiArray;
 
     expect(array).toEqual(new KopiArray([
       new KopiNumber(1),
@@ -125,8 +123,8 @@ describe('Array', () => {
     ]));
 
     var array = await interpret(`
-    [1, 2, 3] | set 0..1 5
-  `) as KopiArray;
+      [1, 2, 3] | set 0..1 5
+    `) as KopiArray;
 
     expect(array).toEqual(new KopiArray([
       new KopiNumber(5),
@@ -136,8 +134,8 @@ describe('Array', () => {
 
   test('Update', async () => {
     var array = await interpret(`
-    [1, 2, 3] | update 1 (n) => n + 3
-  `) as KopiArray;
+      [1, 2, 3] | update 1 (n) => n + 3
+    `) as KopiArray;
 
     expect(array).toEqual(new KopiArray([
       new KopiNumber(1),
@@ -148,8 +146,8 @@ describe('Array', () => {
 
   test('Join', async () => {
     var string = await interpret(`
-    ["a", "b", "c"] | joinWith ", "
-  `) as KopiString;
+      ["a", "b", "c"] | joinWith ", "
+    `) as KopiString;
 
     expect(string).toEqual(
       new KopiString('a, b, c')
@@ -158,8 +156,8 @@ describe('Array', () => {
 
   test('Async resolve', async () => {
     var array = await interpret(`
-    [1, sleep 2, 3]
-  `) as KopiArray;
+      [1, sleep 2, 3]
+    `) as KopiArray;
 
     expect(array).toEqual(new KopiArray([
       Promise.resolve(new KopiNumber(1)),
