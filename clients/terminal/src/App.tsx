f7754cf5af942277edac037622f259f1102e0201
Mike Austin <mike_ekim@yahoo.com>
2022-12-11 15:11:56 -0800

    Add history support

--- clients/terminal/src/App.tsx
+++ clients/terminal/src/App.tsx
@@ -26,9 +26,11 @@ function HistoryLine({ type, output }: { type: 'input' | 'output', output: React
 }
 
 function App() {
-  const [history, setHistory] = useState<{ type: 'input' | 'output', line: React.ReactNode; }[]>([]);
-  const [line, setLine] = useState<string>('');
+  const [historyLines, setHistoryLines] = useState<{ type: 'input' | 'output', line: React.ReactNode; }[]>([]);
+  const [historyIndex, setHistoryIndex] = useState<number>(-1);
+  const [currentLine, setLine] = useState<string>('');
 
+  const history = useRef<string[]>([]);
   const appElementRef = useRef<HTMLDivElement>(null);
   const inputElementRef = useRef<HTMLInputElement>(null);
 
@@ -46,28 +48,48 @@ function App() {
 
   const handleInputKeyDown = async (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === 'Enter') {
-      setHistory(history => [...history, { type: 'input', line: line + ' ' }]);
+      setHistoryLines(historyLines => [...historyLines, { type: 'input', line: currentLine + ' ' }]);
+      history.current.push(currentLine);
+      setHistoryIndex(-1);
       setLine('');
 
-      const value = await interpret(line);
+      const value = await interpret(currentLine);
 
       if (React.isValidElement((value as any)?.element)) {
-        setHistory(history => [...history, { type: 'output', line: (value as any).element }]);
+        setHistoryLines(historyLines => [...historyLines, { type: 'output', line: (value as any).element }]);
 
         return;
       }
 
       const inspect = await value.inspect();
 
-      setHistory(history => [...history, { type: 'output', line: inspect + ' ' }]);
+      setHistoryLines(historyLines => [...historyLines, { type: 'output', line: inspect + ' ' }]);
+    } else if (event.key === 'ArrowUp') {
+      event.preventDefault();
+
+      setHistoryIndex(historyIndex => historyIndex < history.current.length ? historyIndex + 1 : historyIndex);
+    } else if (event.key === 'ArrowDown') {
+      event.preventDefault();
+
+      setHistoryIndex(historyIndex => historyIndex > 0 ? historyIndex - 1 : historyIndex);
     }
   };
+
+  useEffect(() => {
+    const item = history.current[history.current.length - historyIndex];
+
+    if (item === undefined) {
+      setLine('');
+    } else {
+      setLine(item);
+    }
+  }, [historyIndex]);
 
   useEffect(() => {
     if (appElementRef.current) {
       appElementRef.current.scrollTop = appElementRef.current.scrollHeight - appElementRef.current.clientHeight;
     }
-  }, [history]);
+  }, [historyLines]);
 
   return (
     <View
@@ -79,7 +101,7 @@ function App() {
       onPointerDown={handleTerminalPointerDown}
     >
       <View flex style={{ justifyContent: 'flex-end' }}>
-        {history.map((line, index) => (
+        {historyLines.map((line, index) => (
           <HistoryLine key={index} type={line.type} output={line.line} />
         ))}
       </View>
@@ -88,7 +110,7 @@ function App() {
         <Spacer size="xsmall" />
         <input
           ref={inputElementRef}
-          value={line}
+          value={currentLine}
           style={{ flex: 1, border: 0, padding: 0, outline: 'none', fontFamily: 'source-code-pro, Menlo, Monaco, Consolas, monospace', fontSize: 14, lineHeight: '20px', background: 'transparent' }}
           onKeyDown={handleInputKeyDown}
           onChange={handleInputChange}
